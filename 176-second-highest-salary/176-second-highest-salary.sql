03c4d0965ebe8291ee25a4d45af11b5f803c4c87
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-01-22 18:12:08 -0600

    Time: 322 ms (13.64%), Space: 0B (100.00%) - LeetHub

---
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# SELECT A.salary AS SecondHighestSalary FROM (SELECT id, salary, RANK() OVER (ORDER BY salary DESC) 
+#                       AS rank_v FROM Employee) AS A 
+#                       WHERE A.rank_v = 2
+SELECT CASE WHEN MAX(rank_v) = 1 THEN null
+ELSE A.salary END AS SecondHighestSalary
+FROM (SELECT id, salary, RANK() OVER (ORDER BY salary DESC) 
+                      AS rank_v FROM Employee) AS A 
+                      WHERE A.rank_v = 2
