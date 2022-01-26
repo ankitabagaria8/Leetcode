03fdeca8c402357b81343bff663ee19b3e4cd515
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-01-25 18:55:36 -0600

    Time: 55 ms (16.67%), Space: 14.2 MB (48.70%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        x = ''
+        for i in digits:
+            x+= str(i)
+        y = int(x)
+        z = str(y+1)
+        print(z)
+        res = []
+        for j in z:
+            res.append(int(j))
+        return res
