fbe29a37133334e02a64a1fb02902b86910276d9
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-05-08 19:22:29 -0500

    Time: 709 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -3,3 +3,4 @@ SELECT A.sale_date, A.diff FROM
 (SELECT sale_date, fruit, LAG(sold_num) OVER (partition by sale_date Order by sale_date) - sold_num AS diff
 FROM Sales) AS A
 WHERE A.diff IS NOT NULL
+
