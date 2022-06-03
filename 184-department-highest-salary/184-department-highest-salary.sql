d1dbb467f5d64274cf96a8401a107540d73368f6
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-02 22:13:18 -0500

    Time: 542 ms (83.00%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,11 +1,46 @@
 # Write your MySQL query statement below
-
-SELECT A.Department, A.Employee, A.salary
-FROM 
-(SELECT E.name AS Employee, D.name AS Department, salary, RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS ranking
+SELECT dept_name AS Department, A.name AS Employee, A.salary AS Salary FROM
+(SELECT E.id AS emp_id, E.name, E.salary, D.id AS dept_id, D.name AS dept_name,
+ RANK() OVER (PARTITION BY D.id ORDER BY salary DESC) AS ranking
 FROM Employee AS E
 JOIN Department AS D
-ON E.departmentId =D.id) AS A
+ON E.departmentId = D.id) AS A
 WHERE ranking = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT A.Department, A.Employee, A.salary
+# FROM 
+# (SELECT E.name AS Employee, D.name AS Department, salary, RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS ranking
+# FROM Employee AS E
+# JOIN Department AS D
+# ON E.departmentId =D.id) AS A
+# WHERE ranking = 1
 
 
