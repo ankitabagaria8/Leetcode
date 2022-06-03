f7686db7d787186b10f994db1f40580990a29744
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-03 18:54:43 -0500

    Time: 365 ms (82.81%), Space: 0B (100.00%) - LeetHub

--- 614-second-degree-follower/614-second-degree-follower.sql
+++ 614-second-degree-follower/614-second-degree-follower.sql
@@ -1,5 +1,44 @@
 # Write your MySQL query statement below
-SELECT followee AS follower, COUNT(followee) AS num FROM Follow 
-WHERE followee IN (SELECT follower FROM Follow)
-GROUP BY followee
-ORDER BY followee
+
+SELECT follower, cnt2 AS num FROM
+(SELECT follower, IFNULL(COUNT(followee),0) AS cnt1
+FROM Follow 
+GROUP BY follower) AS A
+JOIN
+(SELECT followee, IFNULL(COUNT(follower),0) AS cnt2
+FROM Follow
+GROUP BY followee) AS B
+ON A.follower = B.followee
+ORDER BY follower
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT followee AS follower, COUNT(followee) AS num FROM Follow 
+# WHERE followee IN (SELECT follower FROM Follow)
+# GROUP BY followee
+# ORDER BY followee
