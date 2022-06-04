e6a749b17ef3f8660b8c1ea6ea5f50e0c521469c
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-04 15:04:14 -0500

    Time: 936 ms (7.55%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -1,4 +1,46 @@
 # Write your MySQL query statement below
+# SELECT DISTINCT page_id AS recommended_page FROM
+# (SELECT page_id
+# FROM Friendship AS F
+# JOIN Likes AS L
+# ON F.user2_id = L.user_id
+# AND F.user2_id = L.user_id
+# WHERE user1_id = 1 OR user2_id = 1
+# AND page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1)) AS A
+
+
+# UNION 
+# SELECT user2_id, user1_id, page_id
+# FROM Friendship AS F
+# JOIN Likes AS L
+# ON F.user1_id = L.user_id 
+# WHERE user2_id = 1
+# AND page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1)) AS A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 SELECT DISTINCT page_id AS recommended_page
