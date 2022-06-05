7c3e7a5746c1add7ade5a98e13f35a3e7965a2c2
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-04 20:31:42 -0500

    Time: 572 ms (73.12%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,7 +1,39 @@
 # Write your MySQL query statement below
-SELECT A.person1, A.person2, COUNT(duration) AS call_count, SUM(duration) AS total_duration FROM
+SELECT person1, person2, COUNT(duration) AS call_count, SUM(duration) AS total_duration
+FROM
 (SELECT (CASE WHEN from_id < to_id THEN from_id ELSE to_id END) AS person1, 
-(CASE WHEN to_id > from_id THEN to_id ELSE from_id END) AS person2, 
-duration
+(CASE WHEN to_id > from_id THEN to_id ELSE from_id END) AS person2, duration
 FROM Calls) AS A
-GROUP BY A.person1, A.person2
+GROUP BY person1, person2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT A.person1, A.person2, COUNT(duration) AS call_count, SUM(duration) AS total_duration FROM
+# (SELECT (CASE WHEN from_id < to_id THEN from_id ELSE to_id END) AS person1, 
+# (CASE WHEN to_id > from_id THEN to_id ELSE from_id END) AS person2, 
+# duration
+# FROM Calls) AS A
+# GROUP BY A.person1, A.person2
