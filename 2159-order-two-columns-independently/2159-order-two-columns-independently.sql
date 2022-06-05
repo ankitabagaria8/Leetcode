b47cd168d351c7fa649a6ab8bdb855936b239156
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-05 12:09:50 -0500

    Time: 631 ms (11.50%), Space: 0B (100.00%) - LeetHub

--- 2159-order-two-columns-independently/2159-order-two-columns-independently.sql
+++ 2159-order-two-columns-independently/2159-order-two-columns-independently.sql
@@ -1,8 +1,20 @@
 # Write your MySQL query statement below
-SELECT D1.first_col, D2.second_col FROM
-(SELECT first_col, ROW_NUMBER() OVER(ORDER BY first_col) AS row1
-FROM Data) AS D1
+
+SELECT first_col, second_col FROM
+(SELECT first_col, ROW_NUMBER() OVER(ORDER BY first_col) AS num1 FROM Data 
+) AS A
 JOIN
-(SELECT second_col, ROW_NUMBER() OVER(ORDER BY second_col DESC) AS row2
-FROM Data) AS D2
-ON D1.row1 = D2.row2
+(SELECT  ROW_NUMBER() OVER(ORDER BY second_col DESC) AS num2, second_col FROM Data) AS B
+ON A.num1 = B.num2
+
+
+
+
+
+# SELECT D1.first_col, D2.second_col FROM
+# (SELECT first_col, ROW_NUMBER() OVER(ORDER BY first_col) AS row1
+# FROM Data) AS D1
+# JOIN
+# (SELECT second_col, ROW_NUMBER() OVER(ORDER BY second_col DESC) AS row2
+# FROM Data) AS D2
+# ON D1.row1 = D2.row2
