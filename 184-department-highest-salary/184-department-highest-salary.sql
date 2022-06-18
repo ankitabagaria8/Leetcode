1e80270272730321a12a1037c1aa8475a5086581
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-17 20:21:38 -0500

    Time: 512 ms (93.16%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,11 +1,44 @@
 # Write your MySQL query statement below
-SELECT dept_name AS Department, A.name AS Employee, A.salary AS Salary FROM
-(SELECT E.id AS emp_id, E.name, E.salary, D.id AS dept_id, D.name AS dept_name,
- RANK() OVER (PARTITION BY D.id ORDER BY salary DESC) AS ranking
-FROM Employee AS E
+SELECT A.name AS Employee, D.name AS Department, A.salary AS Salary FROM
+(SELECT name, salary, departmentId, RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS ranking 
+FROM Employee) AS A
 JOIN Department AS D
-ON E.departmentId = D.id) AS A
-WHERE ranking = 1
+ON A. departmentId = D.id
+WHERE ranking=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT dept_name AS Department, A.name AS Employee, A.salary AS Salary FROM
+# (SELECT E.id AS emp_id, E.name, E.salary, D.id AS dept_id, D.name AS dept_name,
+#  RANK() OVER (PARTITION BY D.id ORDER BY salary DESC) AS ranking
+# FROM Employee AS E
+# JOIN Department AS D
+# ON E.departmentId = D.id) AS A
+# WHERE ranking = 1
 
 
 
