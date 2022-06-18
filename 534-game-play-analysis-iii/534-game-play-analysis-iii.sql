7c55c567df2d4afedfbf913589b608bf2ff0df2e
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-17 20:31:16 -0500

    Time: 614 ms (99.22%), Space: 0B (100.00%) - LeetHub

--- 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
+++ 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
@@ -2,3 +2,39 @@
 SELECT player_id, event_date, 
 SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS games_played_so_far
 FROM Activity 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT player_id, event_date, 
+# SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS games_played_so_far
+# FROM Activity
+
+
+# SELECT player_id, event_date, 
+# SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS games_played_so_far
+# FROM Activity 
