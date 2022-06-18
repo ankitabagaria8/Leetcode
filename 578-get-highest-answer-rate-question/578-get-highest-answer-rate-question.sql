f6fe316200cf22b551472efaf464afa536869ba8
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-17 21:25:16 -0500

    Time: 377 ms (17.00%), Space: 0B (100.00%) - LeetHub

--- 578-get-highest-answer-rate-question/578-get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/578-get-highest-answer-rate-question.sql
@@ -1,14 +1,49 @@
 # Write your MySQL query statement below
-
-
-
-SELECT A.question_id AS survey_log FROM
-(SELECT question_id,  
-sum(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)/sum(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS rate
+SELECT C.question_id AS survey_log FROM 
+(SELECT B.question_id, RANK() OVER (ORDER BY ratio DESC) AS ranking FROM 
+(SELECT A.question_id, 
+A.answer_cnt/A.show_cnt AS ratio FROM
+(SELECT question_id, SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS show_cnt, 
+SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END) AS answer_cnt
 FROM SurveyLog
-GROUP BY question_id
-order by rate DESC, question_id) AS A
+GROUP BY question_id) AS A) AS B) AS C
+WHERE C.ranking = 1
+ORDER BY question_id 
 LIMIT 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT A.question_id AS survey_log FROM
+# (SELECT question_id,  
+# sum(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)/sum(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS rate
+# FROM SurveyLog
+# GROUP BY question_id
+# order by rate DESC, question_id) AS A
+# LIMIT 1
 
 
 
