ca67297ffbf87f4259e761a0a3c9690e154963b6
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-06-19 15:39:52 -0500

    Time: 361 ms (43.88%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,6 +1,34 @@
 # Write your MySQL query statement below
-SELECT A.sale_date, A.diff FROM 
-(SELECT sale_date, fruit, LAG(sold_num) OVER (partition by sale_date Order by sale_date) - sold_num AS diff
+
+SELECT sale_date, diff FROM
+(SELECT sale_date, sold_num - LEAD(sold_num) OVER (PARTITION BY sale_date) AS diff
 FROM Sales) AS A
-WHERE A.diff IS NOT NULL
+WHERE diff IS NOT NULL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT sale_date, diff FROM
+# (SELECT sale_date, sold_num - lead(sold_num) OVER(PARTITION BY sale_date ORDER by fruit) AS diff
+# FROM Sales) AS A
+# WHERE diff IS NOT NULL
+
+
+
+# SELECT A.sale_date, A.diff FROM 
+# (SELECT sale_date, fruit, LAG(sold_num) OVER (partition by sale_date Order by sale_date) - sold_num AS diff
+# FROM Sales) AS A
+# WHERE A.diff IS NOT NULL
 
