3ea0f5e34829943a4905852e07118460f20a0f86
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-07-22 20:26:49 -0500

    Time: 464 ms (60.64%), Space: 0B (100.00%) - LeetHub

--- 574-winning-candidate/574-winning-candidate.sql
+++ 574-winning-candidate/574-winning-candidate.sql
@@ -1,15 +1,85 @@
-SELECT A.name FROM 
-(SELECT  C.name, COUNT(C.id) AS cnt
-FROM Candidate AS C
-JOIN Vote AS V
-ON C.id = V.candidateId
-GROUP BY C.name
-ORDER BY COUNT(C.id) DESC) AS A
-LIMIT 1
+SELECT name AS Name FROM Candidate AS C
+JOIN
+(SELECT candidateId, COUNT(*) AS cnt
+FROM Vote 
+GROUP BY candidateId
+ORDER BY cnt DESC
+LIMIT 1) AS A
+ON A.candidateId = C.id
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT A.name FROM 
+# (SELECT name, COUNT(V.id) AS cnt
+# FROM Candidate AS C
+# JOIN Vote AS V
+# ON C.id = V.candidateId
+# GROUP BY name
+# ORDER BY cnt DESC
+# LIMIT 1) AS A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT A.name FROM 
+# (SELECT  C.name, COUNT(C.id) AS cnt
+# FROM Candidate AS C
+# JOIN Vote AS V
+# ON C.id = V.candidateId
+# GROUP BY C.name
+# ORDER BY COUNT(C.id) DESC) AS A
+# LIMIT 1
 
 
 
