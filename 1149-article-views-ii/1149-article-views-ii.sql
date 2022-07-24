b46718033f40992b24512fcb2c5b3e3497a08cdd
Ankita Bagaria <ankita.bagaria8@gmail.com>
2022-07-23 20:52:38 -0500

    Time: 980 ms (9.96%), Space: 0B (100.00%) - LeetHub

--- 1149-article-views-ii/1149-article-views-ii.sql
+++ 1149-article-views-ii/1149-article-views-ii.sql
@@ -1,6 +1,79 @@
 # Write your MySQL query statement below
-SELECT distinct A.viewer_id as id FROM (SELECT viewer_id, view_date, COUNT(DISTINCT article_id) AS counted
+SELECT DISTINCT viewer_id AS id FROM 
+(SELECT viewer_id, COUNT(DISTINCT article_id) AS cnt
 FROM Views 
-group by viewer_id, view_date
-HAVING counted > 1) AS A
+GROUP BY viewer_id, view_date
+HAVING cnt >= 2) AS A
 ORDER BY viewer_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT distinct viewer_id AS id FROM
+# (SELECT viewer_id, view_date
+# FROM Views 
+# GROUP BY viewer_id, view_date
+# HAVING COUNT(DISTINCT article_id) > 1) AS A
+# ORDER BY id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT distinct A.viewer_id AS id FROM
+# (SELECT viewer_id, view_date, COUNT(DISTINCT article_id) AS cnt
+# FROM Views AS V
+# GROUP BY viewer_id, view_date
+# HAVING cnt > 1) AS A
+# ORDER BY id
+
+
+
+
+
+# SELECT distinct A.viewer_id as id FROM (SELECT viewer_id, view_date, COUNT(DISTINCT article_id) AS counted
+# FROM Views 
+# group by viewer_id, view_date
+# HAVING counted > 1) AS A
+# ORDER BY viewer_id
